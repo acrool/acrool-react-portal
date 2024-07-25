347695809e45d7395ed97f7786237b50664cf2ad
imagine10255 <imagine10255@gmail.com>
2024-07-25 09:48:29 +0800

    remove export type

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,2 @@
 export {default} from './ReactPortal';
 export {default as ReactDidMountPortal} from './ReactDidMountPortal';
-export * from './types';
--- src/types.ts
+++
@@ -1,1 +0,0 @@
-export type TContainerSelector = () => HTMLElement|null
