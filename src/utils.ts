e14c4b8ee38b258a9b747ab202223cf85218ffbf
imagine10255 <imagine10255@gmail.com>
2025-05-01 20:52:09 +0800

    add support nextjs

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,23 @@
+import {IPortalProps} from './types';
+
 /**
  * Object.keys 型別增強
  * @param object
  */
 export function objectKeys<T extends object>(object: T): Array<keyof T> {
     return Object.keys(object) as Array<keyof T>;
+}
+
+export function createDiv (props: IPortalProps): HTMLElement {
+    const el = document.createElement('div');
+    el.id = props.id;
+    if(props.className){
+        el.className = props.className;
+    }
+    if(props.style){
+        objectKeys(props.style).forEach(key => {
+            el.style[key] = props.style?.[key];
+        });
+    }
+    return el;
 }
